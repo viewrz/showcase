c27bac4c21097a49716985d0f3a2b8bb6ff82c77
guillaumervls <3765057+guillaumervls@users.noreply.github.com>
2018-10-08 19:17:15 +0200

    add logos

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import { Flex, Text, Box, Logo, Button, Fading } from './base-components';
 import Waypoint from 'react-waypoint';
+import { Box, Button, Fading, Flex, Logo, Text } from './base-components';
 import Header from './Header';
+import jbl from './img/logos/jbl.png';
+import mufe from './img/logos/mufe.png';
+import samsung from './img/logos/samsung.png';
+import seiko from './img/logos/seiko.png';
 
 const BG_PATTERN =
   "url(\"data:image/svg+xml,%3Csvg width='180' height='180' viewBox='0 0 180 180' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M81.28 88H68.413l19.298 19.298L81.28 88zm2.107 0h13.226L90 107.838 83.387 88zm15.334 0h12.866l-19.298 19.298L98.72 88zm-32.927-2.207L73.586 78h32.827l.5.5 7.294 7.293L115.414 87l-24.707 24.707-.707.707L64.586 87l1.207-1.207zm2.62.207L74 80.414 79.586 86H68.414zm16 0L90 80.414 95.586 86H84.414zm16 0L106 80.414 111.586 86h-11.172zm-8-6h11.173L98 85.586 92.414 80zM82 85.586L87.586 80H76.414L82 85.586zM17.414 0L.707 16.707 0 17.414V0h17.414zM4.28 0L0 12.838V0h4.28zm10.306 0L2.288 12.298 6.388 0h8.198zM180 17.414L162.586 0H180v17.414zM165.414 0l12.298 12.298L173.612 0h-8.198zM180 12.838L175.72 0H180v12.838zM0 163h16.413l.5.5 7.294 7.293L25.414 172l-8 8H0v-17zm0 10h6.613l-2.334 7H0v-7zm14.586 7l7-7H8.72l-2.333 7h8.2zM0 165.414L5.586 171H0v-5.586zM10.414 171L16 165.414 21.586 171H10.414zm-8-6h11.172L8 170.586 2.414 165zM180 163h-16.413l-7.794 7.793-1.207 1.207 8 8H180v-17zm-14.586 17l-7-7h12.865l2.333 7h-8.2zM180 173h-6.613l2.334 7H180v-7zm-21.586-2l5.586-5.586 5.586 5.586h-11.172zM180 165.414L174.414 171H180v-5.586zm-8 5.172l5.586-5.586h-11.172l5.586 5.586zM152.933 25.653l1.414 1.414-33.94 33.942-1.416-1.416 33.943-33.94zm1.414 127.28l-1.414 1.414-33.942-33.94 1.416-1.416 33.94 33.943zm-127.28 1.414l-1.414-1.414 33.94-33.942 1.416 1.416-33.943 33.94zm-1.414-127.28l1.414-1.414 33.942 33.94-1.416 1.416-33.94-33.943zM0 85c2.21 0 4 1.79 4 4s-1.79 4-4 4v-8zm180 0c-2.21 0-4 1.79-4 4s1.79 4 4 4v-8zM94 0c0 2.21-1.79 4-4 4s-4-1.79-4-4h8zm0 180c0-2.21-1.79-4-4-4s-4 1.79-4 4h8z' fill='%23ffffff' fill-opacity='0.1' fill-rule='evenodd'/%3E%3C/svg%3E\");";
@@ -90,10 +94,12 @@ const Differences = () => (
               Technology
             </Box>
             <Box mt={2}>
-              <Bullet />Bring scalability
+              <Bullet />
+              Bring scalability
             </Box>
             <Box mt={2}>
-              <Bullet />Prevent fraud
+              <Bullet />
+              Prevent fraud
             </Box>
           </Box>
         </Flex>
@@ -112,10 +118,12 @@ const Differences = () => (
                 Standardised processes
               </Box>
               <Box mt={2}>
-                <Bullet />Guarantee quality of delivery
+                <Bullet />
+                Guarantee quality of delivery
               </Box>
               <Box mt={2}>
-                <Bullet />Offer clear campaign insights
+                <Bullet />
+                Offer clear campaign insights
               </Box>
             </Box>
           </Flex>
@@ -154,13 +162,16 @@ const FullService = () => (
           </Box>
           <Box>
             <Box>
-              <Bullet />Recommendations
+              <Bullet />
+              Recommendations
             </Box>
             <Box>
-              <Bullet />Creative input
+              <Bullet />
+              Creative input
             </Box>
             <Box>
-              <Bullet />Best practice sharing
+              <Bullet />
+              Best practice sharing
             </Box>
           </Box>
         </Box>
@@ -170,10 +181,12 @@ const FullService = () => (
           </Box>
           <Box>
             <Box>
-              <Bullet />From brief to reporting
+              <Bullet />
+              From brief to reporting
             </Box>
             <Flex>
-              <Bullet />Centralised influencer pool management
+              <Bullet />
+              Centralised influencer pool management
             </Flex>
           </Box>
         </Box>
@@ -182,10 +195,12 @@ const FullService = () => (
             Reporting
           </Box>
           <Box>
-            <Bullet />Analytics
+            <Bullet />
+            Analytics
           </Box>
           <Flex>
-            <Bullet />Documentation & screenshots
+            <Bullet />
+            Documentation & screenshots
           </Flex>
         </Box>
       </Flex>
@@ -215,6 +230,27 @@ const FullService = () => (
           </Text>
           <Text mx={2}>
             <i className="ion-logo-twitch" />
+          </Text>
+        </Flex>
+      </Box>
+      <Box mt={3} ta="center" fontSize={5}>
+        <Box>They trusted us:</Box>
+        <Flex wrap justify="center" alignItems="center" py={2}>
+          <Text m={2}>
+            <img src={samsung} style={{ width: '100px' }} alt="Samsung" />
+          </Text>
+          <Text m={2}>
+            <img
+              src={jbl}
+              style={{ width: '100px', marginBottom: '-25px' }}
+              alt="JBL"
+            />
+          </Text>
+          <Text m={2}>
+            <img src={seiko} style={{ width: '100px' }} alt="Seiko" />
+          </Text>
+          <Text m={2}>
+            <img src={mufe} style={{ width: '100px' }} alt="Make Up For Ever" />
           </Text>
         </Flex>
       </Box>
@@ -266,16 +302,20 @@ class HomeHeader extends Component {
           fontWeight="black"
         >
           <Box>
-            <Bullet />Scalable
+            <Bullet />
+            Scalable
           </Box>
           <Box>
-            <Bullet />Measurable
+            <Bullet />
+            Measurable
           </Box>
           <Box>
-            <Bullet />Fraud free
+            <Bullet />
+            Fraud free
           </Box>
           <Box>
-            <Bullet />Professional
+            <Bullet />
+            Professional
           </Box>
         </Flex>
         <Box>
